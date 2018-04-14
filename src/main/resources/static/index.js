63dde9a1d13e4cf1308f5912ab5c97ccfea74e5f
刘培振 <liupeizhen@bestvike.com>
2018-04-14 22:33:12 +0800

    uu

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,12 @@
+require.config({
+    baseUrl: "pub",
+    paths: {
+        "jquery": "/jquery/jquery-3.2.1.min.js",
+        "bootstrap": "/bootstrap-3.3.7-dist/js/bootstrap.min.js",
+        "less": "/less/less.js"
+    }
+});
+
+
+
+// alert("heheh");
